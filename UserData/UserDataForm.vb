08af3f345675a9ebd377590280606d7b3f916c60
rosstimo <rosstimo@isu.edu>
2021-03-11 08:54:19 -0700

    add clean data array

--- UserData/UserDataForm.vb
+++ UserData/UserDataForm.vb
@@ -1,5 +1,6 @@
 ﻿Public Class UserDataForm
     Dim rawData() As String
+    Dim cleanData(6, 199) As String
 
     Sub Junker()
         Dim tempString As String '= "   234    223    343"
@@ -30,12 +31,43 @@
             'DisplayListBox.Items.Add(currentRecord)
             dataString &= currentRecord
         Loop
-        rawData = Split(dataString, ",")
+        rawData = Split(dataString, "$$") ' Chr(34) & "," & Chr(34) & "$$")
+
         FileClose(fileNumber)
+    End Sub
+
+    Sub DisplayArrayContents()
+        Dim temp() As String
+        For i = 1 To UBound(rawData)
+            temp = Split(rawData(i), Chr(34))
+            DisplayListBox.Items.Add(temp(0))
+        Next
+        Me.Text = CStr(DisplayListBox.Items.Count)
+    End Sub
+
+    Sub LoadCleanArray()
+        Dim temp() As String
+        Dim records() As String
+        For i = 1 To UBound(rawData)
+            temp = Split(rawData(i), Chr(34))
+
+            records = Split(temp(0), ",")
+
+            cleanData(0, i) = records(0)
+            cleanData(1, i) = records(1)
+            cleanData(2, i) = ""
+            cleanData(3, i) = records(2)
+            cleanData(4, i) = ""
+            cleanData(5, i) = ""
+            cleanData(6, i) = records(3)
+
+        Next
+
     End Sub
 
     Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
         LoadFile()
+        DisplayArrayContents()
         'Junker()
     End Sub
 End Class
